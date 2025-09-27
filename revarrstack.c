e829169ad954007a3088d524b48038df877f6095
prathamp-cs-tech <prathamp.cs24@bmsce.ac.in>
2025-09-27 09:52:07 +0530

    Add files via upload

---
+++ revarrstack.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+int arr[5];
+int maxsize=4;
+int top=-1;
+void push(int i){
+    if (top==maxsize){
+          printf("The stack will overflow\n");
+    }else{
+        top++;
+        arr[top]=i;
+    }
+}
+int pop(){
+    if (top==-1){
+        printf("The stack will underflow\n");
+        return -1;
+    }
+    else{
+        int item=arr[top];
+        top--;
+        return item;
+    }
+}
+void display(){
+    if (top==-1){
+        printf("The stack will underflow\n");
+    }
+    else{
+        for(int i=top;i>=0;i--){
+            printf("%d\n",arr[i]);
+        }
+    }
+}
+void rev(){
+    for(int i=0;i<=top;i++){
+            printf("%d\n",arr[i]);
+        }
+}
+
+int main(){
+    int j=-1;
+    while(j!=5){
+        printf("Choose 1.push 2.pop 3.display 4.rev 5.stop\n");
+        scanf("%d",&j);
+        if(j==1){
+            int k;
+            scanf("%d",&k);
+            push(k);
+        }
+        else if(j==2){
+            printf("%d\n",pop());
+        }
+        else if(j==3){
+            display();
+        }
+        else if(j==4){
+            rev();
+        }
+        else if(j==5){
+            break;
+        }
+        else{
+            printf("Enter a valid number\n");
+        }
+    }
+}
