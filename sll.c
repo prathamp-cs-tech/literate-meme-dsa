3e98d61c6a902725d83c65ee0af3bba710ac7982
prathamp-cs-tech <prathamp.cs24@bmsce.ac.in>
2025-11-28 23:19:41 +0530

    Update sll.c

--- sll.c
+++ sll.c
@@ -1,81 +1,79 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-struct node{
+struct node {
     int info;
     struct node *next;
 };
 
-struct node * createsll(){
-    struct node *start=NULL, *p;
+struct node* createsll() {
+    struct node *start = NULL, *p;
     int item;
-    printf("Enter element to insert(-999 to stop): ");
-    scanf("%d",&item);
-    while(item!=-999){
-        p=(struct node *)malloc(sizeof(struct node));
-        p->info=item;
-        if (start==NULL){
-            p->next=NULL;
-            start=p;
-        }
-        else{
-            p->next=start;
-            start=p;
-        }
-        scanf("%d",&item);
+    printf("Enter elements (-999 to stop): ");
+    while (scanf("%d", &item) && item != -999) {
+        p = malloc(sizeof(struct node));
+        p->info = item;
+        p->next = start;
+        start = p;
     }
     return start;
 }
 
-struct node * insertatanypos(struct node *start, int item, int opt){
-    struct node *p, *temp, *follow;
-    int pos;
-    p=(struct node *)malloc(sizeof(struct node));
-    p->info=item;
-    if(start==NULL){
-        p->next=NULL;
-        start=p;
-        printf("Sll was empty inserted at start");
+void display(struct node *start) {
+    for (; start; start = start->next) printf("%d ", start->info);
+    printf("\n");
+}
+
+struct node* insert(struct node *start, int item, int posType) {
+    struct node *p = malloc(sizeof(struct node)), *temp = start, *prev = NULL;
+    int pos, i;
+
+    p->info = item;
+
+    if (!start || posType == 1) {      
+        p->next = start;               
+        return p;                      
     }
-    else{   
-        if (opt==1){
-            p->next=start;
-            start=p;
-        }
-        else if(opt==2){
-            printf("Enter pos");
-            scanf("%d",&pos);
-            temp=start;
-            for (int i=0;i<pos;i++){
-                if(temp->next==NULL){
-                    printf("The sll ends at %d while, you wanted to insert at %d, inserting at the end of the sll instead",i,pos);
-                    break;
-                }
-                follow=temp;
-                temp=temp->next;
-            }
-            follow->next=p;
-            p->next=temp;
-        }
-        else if(opt==3){
-            temp=start;
-            while(temp->next!=NULL){
-                temp=temp->next;
-            }
-            temp->next=p;
-            p->next=NULL;
-        }
+
+    if (posType == 3) {                
+        while (temp->next) temp = temp->next;
+        temp->next = p;
+        p->next = NULL;
+        return start;
     }
+
+    printf("Enter position: ");
+    scanf("%d", &pos);
+
+    for (i = 0; i < pos && temp; i++) {     
+        prev = temp;
+        temp = temp->next;
+    }
+
+    prev->next = p;
+    p->next = temp;
     return start;
 }
 
-void display(struct node *start){
-    if (start==NULL){
-        printf("Empty");
-    }
-    else{
-        temp=start;
-        while(temp!=NULL){
-            printf("%d")
+int main() {
+    struct node *start = NULL;
+    int ch, item, opt;
+
+    while (1) {
+        printf("\n1.Create  2.Insert  3.Display  4.Exit\nChoice: ");
+        scanf("%d", &ch);
+
+        if (ch == 1) start = createsll();
+        else if (ch == 2) {
+            printf("Item: ");
+            scanf("%d", &item);
+            printf("1.Beg  2.Pos  3.End : ");
+            scanf("%d", &opt);
+            start = insert(start, item, opt);
         }
+        else if (ch == 3) display(start);
+        else if (ch == 4) break;
+        else printf("Invalid.\n");
     }
+    return 0;
 }
